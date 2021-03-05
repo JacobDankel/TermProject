e1f269b16975a54f51a221d7f605821def434f93
Jacob <jdank99@gmail.com>
2021-03-04 18:05:34 -0800

    initial commit

---
+++ Assets/Scrpits/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -10f);
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -10f);
+    }
+}
---
+++ Assets/Scrpits/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int speed;
+    public int jumpHeight;
+    Rigidbody2D bod;
+    private bool isJumping;
+
+    private void Awake()
+    {
+        bod = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.W) || (Input.GetKeyDown(KeyCode.Space))) && !isJumping)
+        {
+            bod.AddForce(Vector2.up * jumpHeight);
+            isJumping = true;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            bod.AddForce(Vector2.left * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            //bod.AddForce(Vector2.down);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            bod.AddForce(Vector2.right * speed);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    { 
+        if (collision.gameObject.tag == "Floor" )
+        {
+            isJumping = false;
+        }
+    }
+}
