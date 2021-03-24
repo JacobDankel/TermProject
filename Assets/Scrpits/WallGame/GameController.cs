bcb2bd147604985260631a549ebe47d3465db975
Jacob <jdank99@gmail.com>
2021-03-23 17:51:14 -0700

    New Scene
    Added a new WallGame scene and a bunch of different things

---
+++ Assets/Scrpits/WallGame/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Camera playerCam;
+    public Camera sceneCam;
+    [Space]
+    public float timeScale;
+
+    private void Start()
+    {
+        playerCam.enabled = false;
+        sceneCam.GetComponent<AudioListener>().enabled = true;
+        sceneCam.enabled = true;
+        playerCam.GetComponent<AudioListener>().enabled = false;
+        timeScale = Time.deltaTime;
+    }
+
+    public void Update()
+    {
+        if (Application.isEditor)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                sceneCam.GetComponent<AudioListener>().enabled = !sceneCam.GetComponent<AudioListener>().enabled;
+                sceneCam.enabled = !sceneCam.enabled;
+                playerCam.GetComponent<AudioListener>().enabled = !playerCam.GetComponent<AudioListener>().enabled;
+                playerCam.enabled = !playerCam.enabled;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scrpits/WallGame/PlayerCameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    public Transform player;
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, player.position.z - 10);
+    }
+}
